760648072c5c4a824f499ea0775fc871648ee132
Mehul-Hadiyal <mehulhadiyal608@gmail.com>
2025-07-04 23:23:15 +0530

    Empty Test Case : Returns  0

--- src/main/java/org/incubytes/StringCalculator.java
+++ src/main/java/org/incubytes/StringCalculator.java
@@ -1,17 +1,9 @@
 package org.incubytes;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class StringCalculator {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class StringCalculator
+{
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public int add(String number) {
+          return  0;
     }
 }
--- src/test/java/incubytes/StringCalculatorTest.java
+++ src/test/java/incubytes/StringCalculatorTest.java
@@ -1,7 +1,18 @@
 package incubytes;
 
+import org.incubytes.Main;
+import org.incubytes.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
 
     @Test
+    void shouldReturnZeroForEmptyString(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0,calculator.add(""));
+    }
+
 
 }
