993e6f2a44a917f132d07f2b2bde7b13324e97a6
Mehul-Hadiyal <mehulhadiyal608@gmail.com>
2025-07-05 07:48:43 +0530

    [RED] Add failing test for unknown amount of numbers

--- src/test/java/incubytes/StringCalculatorTest.java
+++ src/test/java/incubytes/StringCalculatorTest.java
@@ -30,6 +30,13 @@ public class StringCalculatorTest {
          StringCalculator calculator = new StringCalculator();
          assertEquals(50,calculator.add("5,10,15,20"));
     }
+    @Test
+    public void testUnknownAmountOfNumbersReturnsSum() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("10,20,30,40,50,60,70,80,90,100");
+        assertEquals(550, result);
+    }
+
 
 
 
